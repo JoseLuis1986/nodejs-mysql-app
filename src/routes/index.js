87f15f798bd775115f54f7562e3d0756d6b40023
Jose Luis <joseluissanchezgarcia1986@gmail.com>
2021-09-28 15:19:56 -0400

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    // res.send("Hello world");
+    res.render('index');
+});
+
+
+module.exports = router;
